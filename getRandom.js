37f8f868ff0ae6c447ebf5b8788ed7b6c677f603
faceman7353@gmail.com <faceman7353@gmail.com>
2022-03-22 16:56:14 +0900

    javascript-test

---
+++ getRandom.js
@@ -0,0 +1,7 @@
+export default function random() {
+    return Math.floor(Math.random() * 10)
+}
+
+
+//Math.random()  --- 임의 숫자를 랜덤으로 만드는 함수
+// Math.floor(Math.random() * 10)  ---  올림 내림의 소수점 이하는 전부 버림
